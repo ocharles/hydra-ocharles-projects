58d885dfefc6e35b91569a81a48cf85f1abbc37c
Oliver Charles <ollie@ocharles.org.uk>
2015-01-16 19:39:10 +0000

    Build more things

--- release.nix
+++ release.nix
@@ -6,8 +6,21 @@ let
       digestive-functors-aeson = self.callPackage <digestive-functors-aeson> {};
       ekg-bosun = self.callPackage <ekg-bosun> {};
       ekg-carbon = self.callPackage <ekg-carbon> {};
+      engine-io = self.callPackage <engine-io/engine-io> {};
+      engine-io-snap = self.callPackage <engine-io/engine-io-snap> {};
+      exhaustive = self.callPackage <exhaustive> {};
+      socket-io = self.callPackage <engine-io/socket-io> {};
     };
   };
 in {
-  inherit (haskell) diff3 digestive-functors-aeson ekg-bosun ekg-carbon;
+  inherit (haskell)
+    diff3
+    digestive-functors-aeson
+    ekg-bosun
+    ekg-carbon
+    engine-io
+    engine-io-snap
+    exhaustive
+    socket-io
+    ;
 }
