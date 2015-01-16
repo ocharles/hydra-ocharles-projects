28bc9657f572476be3a6ed02bfddc5e25b325a1c
Oliver Charles <ollie@ocharles.org.uk>
2015-01-16 19:29:13 +0000

    Build ekg-bosun

--- release.nix
+++ release.nix
@@ -4,8 +4,9 @@ let
     overrides = self: super: {
       diff3 = self.callPackage <diff3> {};
       digestive-functors-aeson = self.callPackage <digestive-functors-aeson> {};
+      ekg-bosun = self.callPackage <ekg-bosun> {};
     };
   };
 in {
-  inherit (haskell) diff3 digestive-functors-aeson;
+  inherit (haskell) diff3 digestive-functors-aeson ekg-bosun;
 }
