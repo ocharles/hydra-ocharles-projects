a9d37f721579755ffff9165e5c57c051c491ba6e
Oliver Charles <ollie@ocharles.org.uk>
2015-01-16 18:43:38 +0000

    Build digestive-functors-aeson

--- release.nix
+++ release.nix
@@ -3,8 +3,9 @@ let
   haskell = haskellngPackages.override {
     overrides = self: super: {
       diff3 = self.callPackage <diff3> {};
+      digestive-functors-aeson = self.callPackage <digestive-functors-aeson> {};
     };
   };
 in {
-  inherit (haskell) diff3;
+  inherit (haskell) diff3 digestive-functors-aeson;
 }
