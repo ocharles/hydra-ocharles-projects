39567256a1f1d64a2ebaf787d216f5e161c45b98
Oliver Charles <ollie@ocharles.org.uk>
2015-01-16 16:19:53 +0000

    Build diff3

--- release.nix
+++ release.nix
@@ -1,3 +1,10 @@
 with (import <nixpkgs> {}).pkgs;
-{
+let
+  haskell = haskellngPackages.override {
+    overrides = self: super: {
+      diff3 = self.callPackage <diff3> {};
+    };
+  };
+in {
+  inherit (haskell) diff3;
 }
