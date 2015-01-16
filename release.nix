3045201f7630ce9a08a41900900a3e70fc06dff3
Oliver Charles <ollie@ocharles.org.uk>
2015-01-16 19:30:58 +0000

    Build ekg-carbon

--- release.nix
+++ release.nix
@@ -5,8 +5,9 @@ let
       diff3 = self.callPackage <diff3> {};
       digestive-functors-aeson = self.callPackage <digestive-functors-aeson> {};
       ekg-bosun = self.callPackage <ekg-bosun> {};
+      ekg-carbon = self.callPackage <ekg-carbon> {};
     };
   };
 in {
-  inherit (haskell) diff3 digestive-functors-aeson ekg-bosun;
+  inherit (haskell) diff3 digestive-functors-aeson ekg-bosun ekg-carbon;
 }
