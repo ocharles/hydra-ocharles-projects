34a18a630d465b2afa56be46ef4fc14b0e10cfe8
Oliver Charles <ollie@ocharles.org.uk>
2015-06-24 15:15:01 +0100

    Correct dependency information for libsystemd-journald

--- release.nix
+++ release.nix
@@ -1,10 +1,10 @@
-with (import <nixpkgs> {}).pkgs;
+with (import <nixpkgs> {});
 let
   # The idea here is that every project gets built against the stock `nixpkgs`
   # tree. I think this is better than having a single override, as it much
   # more accurately reflects what a typical `cabal install` will look like.
   project = attr: path: args: {
-    ${attr} = (haskellngPackages.override {
+    ${attr} = (pkgs.haskellngPackages.override {
       overrides = self: super: {
         ${attr} = self.callPackage path args;
       };
@@ -18,7 +18,7 @@ in project "diff3" <diff3> {}
 // project "engine-io-snap" <engine-io/engine-io-snap> {}
 // project "exhaustive" <exhaustive> {}
 // project "json-assertions" <json-assertions> {}
-// project "libsystemd-journal" <libsystemd-journal> {}
+// project "libsystemd-journal" <libsystemd-journal> { systemd = pkgs.systemd; }
 // project "network-carbon" <network-carbon> {}
 // project "pipes-postgresql-simple" <pipes-postgresql-simple> {}
 // project "snap-cors" <snap-cors> {}
